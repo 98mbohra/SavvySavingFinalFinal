f35e8bc1a97326f47b29fca928c6af787724e5fd
matthew <matthewepst@gmail.com>
2019-06-04 22:30:48 +1200

    Settings commenting

--- app/src/main/java/com/example/bohra/savvysavingappfinal/Settings.java
+++ app/src/main/java/com/example/bohra/savvysavingappfinal/Settings.java
@@ -1,3 +1,8 @@
+/*
+This class contains the Java logical code for the settings page
+It allows the tex fields to show the set data that was entered in the app setup process
+ */
+
 package com.example.bohra.savvysavingappfinal;
 
 import android.content.Intent;
@@ -10,50 +15,48 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import org.w3c.dom.Text;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.*;
 
-
-
 public class Settings extends AppCompatActivity
 {
+    //Calender Functionality for selecting savings goal date
     android.icu.util.Calendar currentTime = android.icu.util.Calendar.getInstance();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
 
+        //Setting the layout to be the setting page xml file
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+
+        //Getting the long time from the calender function
         Long minimum = currentTime.getTime().getTime();
 
+        //Deceleration of variables
         final CalendarView savingCalendar = (CalendarView) findViewById(R.id.calendarView);
-        //savingCalendar.setMinDate(minimum);
-
         final EditText goalAmount = (EditText) findViewById(R.id.savingAmountEditText);
         final EditText pledgeAmount = (EditText) findViewById(R.id.pledgeAmountEditText);
-
         Button changeIncome = (Button) findViewById(R.id.buttonChangeIncome);
         Button changeSavings = (Button) findViewById(R.id.buttonChangeIncome);
+        Button changeSettings = (Button) findViewById(R.id.buttonChangeIncome);
 
-        Button changeIncomeButton = (Button) findViewById(R.id.buttonChangeIncome);
-
+        //Creation of the IO class variable to push and pull data to and from the text file.
         IO ioRead = new IO();
 
+        //Arrays that store the users data and show it in the text fields through the xml file
         String[] array = ioRead.readFile("finance.txt").split("\n");
-
         String[] incomeValue = array[0].split(":");
         String[] savingsGoalValue = array[2].split(":");
         String[] pledgeGoalValue = array[6].split(":");
-
         Spinner settingsSpinner = (Spinner)findViewById(R.id.spinnerSettings);
-        //settingsSpinner.setSelection();
 
+        //Setting the text to be viewed in the the xml file
         EditText savingsGoal = (EditText)findViewById(R.id.editText4);
         savingsGoal.setText(savingsGoalValue[1]);
 
@@ -63,19 +66,20 @@ public class Settings extends AppCompatActivity
         EditText pledge = (EditText)findViewById(R.id.editText9);
         pledge.setText(pledgeGoalValue[1]);
 
-        changeIncomeButton.setOnClickListener(new View.OnClickListener()
+        //This is the action listener for the button that changed the vales of the textfields
+        changeSettings.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
                 Spinner pledgePeriodSpinner = (Spinner) findViewById(R.id.periodSpinner3);
-                //Period incomePeriod = Period.valueOf(incomePeriodSpinner.getSelectedItem().toString());
                 String goalCheck = goalAmount.getText().toString();
                 String pledgeCheck = pledgeAmount.getText().toString();
 
+                //Constraints to ensure sure users input is correct
                 if ((goalCheck.isEmpty()) || (pledgeCheck.isEmpty()))
                 {
-                    Toast goalLowToast = Toast.makeText(getApplicationContext(), "Your must set a pledge and goal!", Toast.LENGTH_SHORT);
+                    Toast goalLowToast = Toast.makeText(getApplicationContext(), "You must have values in the text areas!", Toast.LENGTH_SHORT);
                     goalLowToast.show();
                 }
                 else
@@ -87,18 +91,12 @@ public class Settings extends AppCompatActivity
 
                     if ((goalAmountToSend < 0) || (pledgeAmountToSend < 0))
                     {
-                        Toast goalLowToast = Toast.makeText(getApplicationContext(), "Your must set a pledge and goal!", Toast.LENGTH_SHORT);
+                        Toast goalLowToast = Toast.makeText(getApplicationContext(), "You must have values in the text areas!", Toast.LENGTH_SHORT);
                         goalLowToast.show();
                     }
-
-                    //This is the calender function that is used to allow user to set a saving goal date in a visually
-                    //gratifying way.
                     else
                     {
-                        //Date d = new Date(Long.parseLong(String.valueOf(savingCalendar.getDate())));
-                        //SimpleDateFormat df2 = new SimpleDateFormat("dd/MM/yy");
-                        //String dateText = df2.format(d);
-
+                        //Adding and appending the text file with the new information declared by the user
                         String content = "Goal:" + goalAmount.getText().toString() + "\n" +
                                 "Target Date Long:" + savingCalendar.getDate() + "\n" +
                                 "Readable Date:" + savingCalendar.getDate() + "\n" +
@@ -115,5 +113,4 @@ public class Settings extends AppCompatActivity
             }
         });
     }
-
 }
